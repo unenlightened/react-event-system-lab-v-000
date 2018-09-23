44b7a401956097ba96f48afea5150cca03f447fb
Chely Ho <adept_inept@yahoo.com>
2018-09-23 11:37:39 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
-// Code Keypad Component Here
+import Keypad, { Component } from 'react';
+
+export default class Keypad extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+  
+  handleClick = () => {
+    this.setState({
+
+    })
+  }
+  
+  render() {
+    return (
+
+    )
+  }
+  
+}
